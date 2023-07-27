a14bab527de041e38c04b5c552a5a640006ea3f7
Johhan Urrutia <johhanurrutia@192.168.1.16>
2023-07-26 23:09:32 -0400

    fix scripts deploy

--- src/database/config.js
+++ src/database/config.js
@@ -4,6 +4,30 @@ dotenv.config();
 
 const {Pool} = pg;
 
-export const pool = new Pool({
+const connectionString = process.env.PG_URL;
+
+export const pool = connectionString
+? new Pool(
+  {
+    connectionString,
+    ssl: {
+      rejectUnauthorized: false
+    },
+    allowExitOnIdle: true,
+  }
+)
+:
+new Pool({
   allowExitOnIdle: true,
 });
+
+try {
+  await pool.query("SELECT NOW()");
+  console.log("Database connected")
+} catch (error) {
+  console.log(error)
+}
+
+/* export const pool = new Pool({
+  allowExitOnIdle: true,
+}); */
